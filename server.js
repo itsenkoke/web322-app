76aee0b0e9e0cd79ef554799192fea58058f3f32
itsenko <jauingjack0927@gmail.com>
2024-06-12 18:06:01 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const storeService = require('./store-service');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + '/public'));
+
+// parse these files to js object
+const items = JSON.parse(fs.readFileSync('./data/items.json', 'utf8'));
+const categories = JSON.parse(fs.readFileSync('./data/categories.json', 'utf8'));
+
+app.get('/', (req, res) => {
+    res.redirect('/about'); // redirect the user to the /about route
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'about.html')); // must return the about.html file from the views folder
+});
+
+// set a route called /shop, activate when user call
+app.get('/shop', (req, res) => {
+    storeService.getPublishedItems()
+    .then(publishedItems => {
+        res.json(publishedItems);
+    })
+    .catch(err => {
+        res.status(500).json({ message: err });
+    });
+});
+
+// same
+app.get('/items', (req, res) => {
+    storeService.getAllItems()
+    .then(items => {
+        res.json(items);
+    })
+    .catch(err => {
+        res.status(500).json({ message: err });
+    });
+});
+
+// same
+app.get('/categories', (req, res) => {
+    storeService.getAllCategories()
+    .then(categories => {
+        res.json(categories); // send data to the client
+    })
+    .catch(err => {
+        res.status(500).json({ message: err });
+    });
+});
+
+app.use((req, res) => {
+    res.status(404).send('Page Not Found');
+});
+
+// initialize storeService and start the server
+storeService.initialize()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Express http server listening on port ${PORT}`); // output port to the console
+        });
+    })
+    .catch(err => {
+        console.error(`Failed to initialize store service: ${err}`);
+    });
---
+++ store-server.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+const path = require('path');
+
+let item = [];
+let categories = [];
+
+function initialize(){
+    return new Promise((fulfilled, rejected) => {
+        try {
+            const itemDate = fs.readFileSync(path.join(__dirname, 'data', 'items.json'), 'utf8');
+            items = JSON.parse(itemData);
+
+            const categories = fs.readFileSync(path.join(__dirname, 'data', 'categories.json'), 'utf8');
+            categories = JSON.parse(categoriesData);
+
+            fulfilled();
+        }catch (err){
+            rejected(err);
+        }
+    })
+}
+
+function getAllItems() {
+    return new Promise((fulfilled, rejected) => {
+        if (items.length > 0) {
+            fulfilled(items);
+        } else {
+            rejected("No items found");
+        }
+    });
+}
+
+function getCategories() {
+    return new Promise((fulfilled, rejected) => {
+        if (categories.length > 0) {
+            fulfilled(categories);
+        } else {
+            rejected("No categories found");
+        }
+    });
+}
+
+function getPublishedItems() {
+    return new Promise((fulfilled, rejected) => {
+        const publishedItems = items.filter(item => item.published);
+        if (publishedItems.length > 0) {
+            fulfilled(publishedItems);
+        } else {
+            rejected("No published items found");
+        }
+    });
+}
+
+module.exports = {
+    initialize,
+    getAllItems,
+    getCategories,
+    getPublishedItems
+};
